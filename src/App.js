54cfa67f086c51934519bc97ea0a36ad368932a0
thaian161 <thaian161@yahoo.com>
2022-09-05 20:58:56 -0500

    add function to check for colum of 4

--- src/App.js
+++ src/App.js
@@ -6,9 +6,57 @@ const candyColors = ['blue', 'green', 'orange', 'purple', 'red', 'yellow'];
 function App() {
   const [currentColorArrangement, setCurrentColorArrangement] = useState([]);
 
+  const checkForColumnOfFour = () => {
+    for (let i = 0; i < 39; i++) {
+      const columnOfFour = [i, i + width, i + width * 2, i + width * 3];
+      const decidedColor = currentColorArrangement[i];
+
+      if (
+        columnOfFour.every(
+          (square) => currentColorArrangement[square] === decidedColor
+        )
+      ) {
+        columnOfFour.forEach(
+          (square) => (currentColorArrangement[square] = '')
+        );
+      }
+    }
+  };
+
   const checkForColumnOfThree = () => {
     for (let i = 0; i < 47; i++) {
-      const columnIfThree = [i, i + width, i + width * 2];
+      const columnOfThree = [i, i + width, i + width * 2];
+      const decidedColor = currentColorArrangement[i];
+
+      if (
+        columnOfThree.every(
+          (square) => currentColorArrangement[square] === decidedColor
+        )
+      ) {
+        columnOfThree.forEach(
+          (square) => (currentColorArrangement[square] = '')
+        );
+      }
+    }
+  };
+
+  const checkForRowOfThree = () => {
+    for (let i = 0; i < 64; i++) {
+      const rowOfThree = [i, i + 1, i + 2];
+      const decidedColor = currentColorArrangement[i];
+      const notValid = [
+        6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55, 63, 64,
+      ];
+
+      if (notValid.includes(i)) continue;
+
+      if (
+        rowOfThree.every(
+          (square) => currentColorArrangement[square] === decidedColor
+        )
+      ) {
+        rowOfThree.forEach((square) => (currentColorArrangement[square] = ''));
+      }
     }
   };
 
@@ -33,6 +81,17 @@ function App() {
   useEffect(() => {
     createBoard();
   }, []);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      checkForColumnOfFour();
+      checkForColumnOfThree();
+
+      setCurrentColorArrangement([...currentColorArrangement]);
+    }, 100);
+
+    return () => clearInterval(timer);
+  }, [checkForColumnOfFour, checkForColumnOfThree, currentColorArrangement]);
 
   console.log(currentColorArrangement);
 
