1b79d0e5664bd7f512c628a3e4754f1d5b6c433b
thaian161 <thaian161@yahoo.com>
2022-09-05 19:28:04 -0500

    complete function to create the board game

--- src/App.js
+++ src/App.js
@@ -2,6 +2,26 @@ const width = 8;
 const candyColors = ['blue', 'green', 'orange', 'purple', 'red', 'yellow'];
 
 function App() {
+  const createBoard = () => {
+    const randomColorArrangement = [];
+
+    for (let i = 0; i < width * width; i++) {
+      /* get randomColor in 2 steps
+      const randomNumberFrom0To5 = Math.floor(
+        Math.random() * candyColors.length
+      );
+      const randomColor = candyColors[randomNumberFrom0To5];
+      */
+
+      const randomColor =
+        candyColors[Math.floor(Math.random() * candyColors.length)];
+      randomColorArrangement.push(randomColor);
+    }
+    console.log(randomColorArrangement);
+  };
+
+  createBoard();
+
   return <div></div>;
 }
 
